51ea75411111ec5e68eac71d83ac122031699477
Infarh <shmachilin@yandex.ru>
2020-05-07 12:50:09 +0300

    Настройка структуры базы данных в EF6

---
+++ EntityFrameworkTest/Data/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+//using System.ComponentModel.DataAnnotations.Schema;
+//using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkTest.Data
+{
+    class Student
+    {
+        //[Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string SurName { get; set; }
+
+        public string Patronymic { get; set; }
+
+        public DateTime Birthday { get; set; }
+
+        //[ForeignKey(nameof(GroupId))]
+        public Group Group { get; set; }
+
+        //public int GroupId { get; set; }
+
+        public ICollection<Course> Courses { get; set; }
+    }
+}
---
+++ EntityFrameworkTest/Data/StudentsDB.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkTest.Data
+{
+    class StudentsDB : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+        public DbSet<Course> Courses { get; set; }
+
+        public DbSet<Group> Groups { get; set; }
+
+        public StudentsDB() : this("name=StudentsDB") { }
+
+        public StudentsDB(string ConnectionString) : base(ConnectionString)
+        {
+
+        }
+    }
+}
